2690ea8540f91c37a31fb3a181a16fb36c96b0e1
Sobak <msobaczewski@gmail.com>
2014-07-08 19:17:58 +0200

    Wiki module is now civilized and it makes use of MediaWiki API

--- services/ServiceWikipedia.php
+++ services/ServiceWikipedia.php
@@ -5,11 +5,10 @@ class ServiceWikipedia implements ServiceInterface {
 	public $url = 'http://pl.wikipedia.org/';
 
 	public function grab($config) {
-		$response = file_get_contents('https://pl.wikipedia.org/w/index.php?title=Specjalna%3AZarz%C4%85dzanie+kontem+uniwersalnym&target='.$config['username']);
-		preg_match('#<li><strong>Wszystkich edycji</strong> (.\d)</li>#', $response, $matches);
+		$user_response = json_decode(file_get_contents('https://pl.wikipedia.org/w/api.php?action=query&list=users&usprop=editcount&format=json&ususers='.$config['username']), true);
 
 		return [
-			'editions' => (int)$matches[1],
+			'editions' => $user_response['query']['users'][0]['editcount'],
 		];
 	}
 
